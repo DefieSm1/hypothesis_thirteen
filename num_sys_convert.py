0c4e7a36f55062c4f1d82890c17ca67088a6f1d0
student <student@ckziu.jaworzno.pl>
2022-12-07 10:06:57 +0100

    add parameter checking

--- num_sys_convert.py
+++ num_sys_convert.py
@@ -1,4 +1,7 @@
 class NumSysConvert:
     def __init__(self, num, count):
+        if 0 > num or num >= 1 or count <= 0:
+            raise Exception("Incorrect number!")
+
         self._num = num
         self._count = count
