6578b8137f5422e7ed502dccf8e957acd85fe214
student <student@ckziu.jaworzno.pl>
2022-12-09 11:00:05 +0100

    .

--- main.py
+++
@@ -1,9 +0,0 @@
-from num_sys_convert import NumSysConvert
-
-def main():
-    convert = NumSysConvert(float(input("podaj liczbe\n")), int(input("podaj ilosc\n")))
-    print(convert.binary())
-    print(convert.ternary())
-
-if __name__ == '__main__':
-    main()
